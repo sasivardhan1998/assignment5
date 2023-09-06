01393ce6a4c2a8a23e48badb42203ef084e55061
sasivardhan1998 <sasivardhsn.marolix@gmail.com>
2023-09-06 16:35:06 +0530

    initial commit

---
+++ fibonacci series.py
@@ -0,0 +1,18 @@
+def fab(n):
+    a=0
+    b=1
+    print(a)
+    print(b)
+    for i in range(2,n):
+        c=a+b
+        a=b
+        b=c
+        print(c)
+
+
+
+
+fab(5)
+result=fab(5)
+print(result)
+  
